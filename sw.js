8e8a44405dc29da92063cce215e644fb0b528b37
Spids91 <keithoreilly8@protonmail.com>
2026-06-18 23:26:17 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE = 'phecc-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => { if(res&&res.status===200&&e.request.method==='GET'){const c=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));} return res; }))); });
+const C='phecc-v3';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['/','/index.html','/manifest.json'])));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res&&res.status===200&&e.request.method==='GET'){const c=res.clone();caches.open(C).then(cache=>cache.put(e.request,c));}return res;})));});
